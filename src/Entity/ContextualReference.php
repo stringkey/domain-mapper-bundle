8bfbea81415f033d1eced1e789a87f5c8263e03a
peer <stringkey@wxs.nl>
2025-01-20 23:14:58 +0100

    Removed Uuid id and accessor from inherting class

--- src/Entity/ContextualReference.php
+++ src/Entity/ContextualReference.php
@@ -7,8 +7,6 @@ namespace Stringkey\MapperBundle\Entity;
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Timestampable\Traits\TimestampableEntity;
 use Stringkey\MapperBundle\Repository\ContextualReferenceRepository;
-use Symfony\Bridge\Doctrine\Types\UuidType;
-use Symfony\Component\Uid\Uuid;
 
 /**
  * @description If you want to create an extended implementation
@@ -19,16 +17,5 @@ use Symfony\Component\Uid\Uuid;
 #[ORM\Entity(repositoryClass: ContextualReferenceRepository::class)]
 final class ContextualReference extends AbstractContextualReference
 {
-//    #[ORM\Id]
-//    #[ORM\Column(type: UuidType::NAME, unique: true)]
-//    #[ORM\GeneratedValue(strategy: 'CUSTOM')]
-//    #[ORM\CustomIdGenerator(class: 'doctrine.uuid_generator')]
-//    private ?Uuid $id = null;
-
     use TimestampableEntity;
-
-    public function getId(): ?Uuid
-    {
-        return $this->id;
-    }
 }
