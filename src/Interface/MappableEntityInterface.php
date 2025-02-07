937092ae1498d9aa0d26e9a4ff007b6f86db66db
stringkey <stringkey@wxs.nl>
2025-02-07 18:01:11 +0100

    Update MappableEntityInterface.php

--- src/Interface/MappableEntityInterface.php
+++ src/Interface/MappableEntityInterface.php
@@ -6,6 +6,15 @@ namespace Stringkey\MapperBundle\Interface;
 
 use Symfony\Component\Uid\Uuid;
 
+/**
+ * This interface is used to flag that an entity is mappable
+ * When using the MappableEntityRegistry, all entities managed by doctrine are listed
+ * The entity name is used to name the groups of the entities
+ * An instance of an entity is being mapped with a record within a group
+ * The group has a reference to the domain object type (fully qualified class name)
+ * The option with in the group has a label and an identifier within that group.
+ */
+
 interface MappableEntityInterface
 {
     public function getId(): Uuid|int|null;
